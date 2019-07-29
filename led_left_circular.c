bf74c4b27d4d52102d5e81893016b6b164bbf7a1
vaasu11 <vaasu.garg1999@gmail.com>
2019-07-29 22:11:49 +0530

    Blinking of 8 LED circularly from left to right

---
+++ led_left_circular.c
@@ -0,0 +1,31 @@
+#include<pic.h>
+void delay(unsigned int x );
+void main()
+{
+TRISC=0x00;
+PORTC=0Xff;
+for(int i=0;;i++)
+{
+ PORTC=0x80;
+ delay(60000);
+ PORTC=0x40;
+ delay(60000);
+ PORTC=0x20;
+ delay(60000);
+ PORTC=0x10;
+ delay(60000);
+ PORTC=0x08;
+ delay(60000);
+ PORTC=0x04;
+ delay(60000);
+ PORTC=0x02;
+ delay(60000);
+ PORTC=0x01;
+ delay(60000);
+}
+}
+void delay(unsigned int x)
+{
+ while(x--);
+   
+}
